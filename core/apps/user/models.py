30977ac367ec799ed54fb7520d9508f6a7c9431f
LMH <90649822+LoneHilke@users.noreply.github.com>
2023-04-06 23:20:56 +0200

    Can't make migrate
    migrations ging now, but not migrate
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- core/apps/user/models.py
+++ core/apps/user/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.utils import timezone
 
 from apps.models import BaseModel
 
 # Create your models here.
+
 class User(AbstractUser, BaseModel):
     has_purchased = models.BooleanField(default=False)
 
